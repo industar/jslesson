f8ba3ac9d23401def4d457d80c1086a9344577f1
Your Name <2708482@gmail.com>
2018-11-07 21:33:54 +0300

    first

---
+++ _clean_html/js/common.js
@@ -0,0 +1,47 @@
+'use strict'
+
+var leftBorderWidth = 1;
+{let second = 2;}
+const pi = 3.14;
+
+console.log (4/3);
+console.log ('string' / 4);
+
+let person = {
+  name: "John",
+  age: 25,
+  isMarried: false
+};
+
+console.log(person["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[0]);
+
+//alert("hello world");
+
+//let answer = confirm("Are you here?");
+
+//console.log(answer);
+
+/*let answer = +prompt("Are you 18?", "Yes");
+
+console.log(typeof(answer));
+
+console.log("arr" + "obj");
+console.log(4 + +"obj");*/
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = false;
+    isClose = false;
+
+console.log(isChecked || !isClose);
